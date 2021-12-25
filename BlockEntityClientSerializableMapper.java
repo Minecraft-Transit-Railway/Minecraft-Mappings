3d9676ec97fda6464fac4bd9a8cb028c976b54f2
Jonathan Ho <jonafanho@gmail.com>
2021-12-25 12:03:42 -0800

    Changed serializable block entity

--- BlockEntityClientSerializableMapper.java
+++ BlockEntityClientSerializableMapper.java
@@ -1,35 +1,34 @@
 package @package@;
 
+import me.shedaniel.architectury.extensions.BlockEntityExtension;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
-public abstract class BlockEntityClientSerializableMapper extends BlockEntityMapper {
+public abstract class BlockEntityClientSerializableMapper extends BlockEntityMapper implements BlockEntityExtension {
 
 	public BlockEntityClientSerializableMapper(BlockEntityType<?> type, BlockPos pos, BlockState state) {
 		super(type, pos, state);
 	}
 
-	public void syncData() {
-		if (level instanceof ServerLevel) {
-			((ServerLevel) level).getChunkSource().blockChanged(worldPosition);
-		}
+	@Override
+	public void loadClientData(BlockState pos, CompoundTag tag) {
+		readCompoundTag(tag);
 	}
 
 	@Override
-	public ClientboundBlockEntityDataPacket getUpdatePacket() {
-		final CompoundTag compoundTag = new CompoundTag();
-		writeCompoundTag(compoundTag);
-		return new ClientboundBlockEntityDataPacket(worldPosition, 127, compoundTag);
+	public CompoundTag saveClientData(CompoundTag tag) {
+		writeCompoundTag(tag);
+		return tag;
 	}
 
-	@Override
-	public final CompoundTag getUpdateTag() {
-		final CompoundTag compoundTag = super.getUpdateTag();
-		writeCompoundTag(compoundTag);
-		return compoundTag;
+	public final void fromClientTag(CompoundTag tag) {
+		readCompoundTag(tag);
+	}
+
+	public final CompoundTag toClientTag(CompoundTag tag) {
+		writeCompoundTag(tag);
+		return tag;
 	}
 }
