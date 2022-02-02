d978d5c93fb7d85ec8fc1785cc19e391a9e427de
Jonathan Ho <jonafanho@gmail.com>
2022-02-01 20:52:24 -0800

    Added add entity packet

--- ForgeUtilities.java
+++ ForgeUtilities.java
@@ -1,11 +1,18 @@
 package @package@;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.world.entity.Entity;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fmllegacy.network.NetworkHooks;
 
 public interface ForgeUtilities {
 
 	static void registerModEventBus(String modId, IEventBus eventBus) {
 		EventBuses.registerModEventBus(modId, eventBus);
+	}
+
+	static Packet<?> createAddEntityPacket(Entity entity) {
+		return NetworkHooks.getEntitySpawningPacket(entity);
 	}
 }
