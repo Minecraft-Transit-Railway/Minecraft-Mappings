ba89b9b593f8a5170736a169f1a296fffcdcb8d7
Jonathan Ho <jonafanho@gmail.com>
2022-02-02 11:23:48 -0800

    Fixed entity renderer registry

--- ForgeUtilities.java
+++ ForgeUtilities.java
@@ -3,8 +3,11 @@ package @package@;
 import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.network.NetworkHooks;
+
+import java.util.function.Function;
 
 public interface ForgeUtilities {
 
@@ -14,5 +17,11 @@ public interface ForgeUtilities {
 
 	static Packet<?> createAddEntityPacket(Entity entity) {
 		return NetworkHooks.getEntitySpawningPacket(entity);
+	}
+
+	static <T extends Entity> void registerEntityRenderer(EntityType<? extends T> entityType, Function<Object, EntityRendererMapper<T>> entityRendererProvider) {
+	}
+
+	class RegisterEntityRenderer {
 	}
 }
