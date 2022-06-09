fd737fc2e4472ef701178402b9b806a1fcd1dd63
Jonathan Ho <jonafanho@gmail.com>
2022-06-08 23:04:23 -0700

    Added a command registry
    # Conflicts:
    #	FabricRegistryUtilities.java

--- FabricRegistryUtilities.java
+++ FabricRegistryUtilities.java
@@ -1,16 +1,20 @@
 package @package@;
 
+import com.mojang.brigadier.CommandDispatcher;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public interface FabricRegistryUtilities {
@@ -25,5 +29,9 @@ public interface FabricRegistryUtilities {
 
 	static <T extends Entity> void registerEntityRenderer(EntityType<T> type, Function<EntityRenderDispatcher, EntityRendererMapper<T>> factory) {
 		EntityRendererRegistry.INSTANCE.register(type, (manager, context) -> factory.apply(manager));
+	}
+
+	static void registerCommand(Consumer<CommandDispatcher<CommandSourceStack>> callback) {
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> callback.accept(dispatcher));
 	}
 }
