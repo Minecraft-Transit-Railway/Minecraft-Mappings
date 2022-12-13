c1cd3cddf99ed5a937abda69ccaf69cbcb77591d
Jonathan Ho <jonafanho@gmail.com>
2022-12-13 15:47:39 -0800

    Added 1.19.3 mappings for Forge

--- ForgeUtilities.java
+++ ForgeUtilities.java
@@ -1,12 +1,24 @@
 package @package@;
 
+import dev.architectury.event.events.client.ClientTextureStitchEvent;
 import dev.architectury.platform.forge.EventBuses;
+import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.core.Registry;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
@@ -14,7 +26,9 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.network.NetworkHooks;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -25,6 +39,7 @@ public class ForgeUtilities {
 	};
 	private static Consumer<Object> renderGameOverlayAction = matrices -> {
 	};
+	private static final Map<ResourceLocation, CreativeModeTab> CREATIVE_MODE_TABS = new HashMap<>();
 	private static final Set<EntityRendererPair<?>> ENTITY_RENDERER_PAIRS = new HashSet<>();
 
 	public static void registerModEventBus(String modId, IEventBus eventBus) {
@@ -39,6 +54,36 @@ public class ForgeUtilities {
 		return NetworkHooks.getEntitySpawningPacket(entity);
 	}
 
+	public static Supplier<CreativeModeTab> createCreativeModeTab(ResourceLocation resourceLocation, Supplier<ItemStack> iconSupplier, String translationKey) {
+		if (!CREATIVE_MODE_TABS.containsKey(resourceLocation)) {
+			CREATIVE_MODE_TABS.put(resourceLocation, CreativeTabRegistry.create(resourceLocation, iconSupplier));
+		}
+		return () -> CREATIVE_MODE_TABS.get(resourceLocation);
+	}
+
+	public static void registerCreativeModeTab(ResourceLocation resourceLocation, Item item) {
+	}
+
+	public static ResourceKey<Registry<Item>> registryGetItem() {
+		return Registry.ITEM_REGISTRY;
+	}
+
+	public static ResourceKey<Registry<Block>> registryGetBlock() {
+		return Registry.BLOCK_REGISTRY;
+	}
+
+	public static ResourceKey<Registry<BlockEntityType<?>>> registryGetBlockEntityType() {
+		return Registry.BLOCK_ENTITY_TYPE_REGISTRY;
+	}
+
+	public static ResourceKey<Registry<EntityType<?>>> registryGetEntityType() {
+		return Registry.ENTITY_TYPE_REGISTRY;
+	}
+
+	public static ResourceKey<Registry<SoundEvent>> registryGetSoundEvent() {
+		return Registry.SOUND_EVENT_REGISTRY;
+	}
+
 	public static void renderTickAction(Runnable runnable) {
 		renderTickAction = runnable;
 	}
@@ -49,6 +94,10 @@ public class ForgeUtilities {
 
 	public static <T extends Entity> void registerEntityRenderer(Supplier<EntityType<? extends T>> entityType, EntityRendererProvider<T> entityRendererProvider) {
 		ENTITY_RENDERER_PAIRS.add(new EntityRendererPair<>(entityType, entityRendererProvider));
+	}
+
+	public static void registerTextureStitchEvent(Consumer<TextureAtlas> consumer) {
+		ClientTextureStitchEvent.POST.register(consumer::accept);
 	}
 
 	public static class Events {
@@ -66,7 +115,7 @@ public class ForgeUtilities {
 		}
 	}
 
-	public static class RegisterEntityRenderer {
+	public static class ClientsideEvents {
 
 		@SubscribeEvent
 		public static void onEntityRendererEvent(EntityRenderersEvent.RegisterRenderers event) {
@@ -87,5 +136,8 @@ public class ForgeUtilities {
 		private void register(EntityRenderersEvent.RegisterRenderers event) {
 			event.registerEntityRenderer(entityTypeSupplier.get(), entityRendererProvider);
 		}
+	}
+
+	public static class RegisterCreativeTabs {
 	}
 }
