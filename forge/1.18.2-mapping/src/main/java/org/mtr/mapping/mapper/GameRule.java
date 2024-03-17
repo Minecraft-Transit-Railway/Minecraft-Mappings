0ec6c586d6d831f746d31cb99b026fa884ec21bc
Jonathan Ho <jonafanho@gmail.com>
2024-03-16 21:53:26 -0700

    Added more mappings 24

---
+++ forge/1.18.2-mapping/src/main/java/org/mtr/mapping/mapper/GameRule.java
@@ -0,0 +1,93 @@
+package org.mtr.mapping.mapper;
+
+import net.minecraft.world.level.GameRules;
+import org.mtr.mapping.annotation.MappedMethod;
+import org.mtr.mapping.holder.MinecraftServer;
+
+import javax.annotation.Nullable;
+
+public enum GameRule {
+
+	ANNOUNCE_ADVANCEMENTS(GameRules.RULE_ANNOUNCE_ADVANCEMENTS, null),
+	BLOCK_EXPLOSION_DROP_DECAY(null, null),
+	COMMAND_BLOCK_OUTPUT(GameRules.RULE_COMMANDBLOCKOUTPUT, null),
+	DISABLE_ELYTRA_MOVEMENT_CHECK(GameRules.RULE_DISABLE_ELYTRA_MOVEMENT_CHECK, null),
+	DISABLE_RAIDS(GameRules.RULE_DISABLE_RAIDS, null),
+	DO_DAYLIGHT_CYCLE(GameRules.RULE_DAYLIGHT, null),
+	DO_ENTITY_DROPS(GameRules.RULE_DOENTITYDROPS, null),
+	DO_FIRE_TICK(GameRules.RULE_DOFIRETICK, null),
+	DO_IMMEDIATE_RESPAWN(GameRules.RULE_DO_IMMEDIATE_RESPAWN, null),
+	DO_INSOMNIA(GameRules.RULE_DOINSOMNIA, null),
+	DO_LIMITED_CRAFTING(GameRules.RULE_LIMITED_CRAFTING, null),
+	DO_MOB_GRIEFING(GameRules.RULE_MOBGRIEFING, null),
+	DO_MOB_LOOT(GameRules.RULE_DOMOBLOOT, null),
+	DO_MOB_SPAWNING(GameRules.RULE_DOMOBSPAWNING, null),
+	DO_PATROL_SPAWNING(GameRules.RULE_DO_PATROL_SPAWNING, null),
+	DO_TILE_DROPS(GameRules.RULE_DOBLOCKDROPS, null),
+	DO_TRADER_SPAWNING(GameRules.RULE_DO_TRADER_SPAWNING, null),
+	DO_VINES_SPREAD(null, null),
+	DO_WARDEN_SPAWNING(null, null),
+	DO_WEATHER_CYCLE(GameRules.RULE_WEATHER_CYCLE, null),
+	DROWNING_DAMAGE(GameRules.RULE_DROWNING_DAMAGE, null),
+	ENDER_PEARLS_VANISH_ON_DEATH(null, null),
+	FALL_DAMAGE(GameRules.RULE_FALL_DAMAGE, null),
+	FIRE_DAMAGE(GameRules.RULE_FIRE_DAMAGE, null),
+	FORGIVE_DEAD_PLAYERS(GameRules.RULE_FORGIVE_DEAD_PLAYERS, null),
+	FREEZE_DAMAGE(GameRules.RULE_FREEZE_DAMAGE, null),
+	GLOBAL_SOUND_EVENTS(null, null),
+	KEEP_INVENTORY(GameRules.RULE_KEEPINVENTORY, null),
+	LAVA_SOURCE_CONVERSION(null, null),
+	LOG_ADMIN_COMMANDS(GameRules.RULE_LOGADMINCOMMANDS, null),
+	MOB_EXPLOSION_DROP_DECAY(null, null),
+	NATURAL_REGENERATION(GameRules.RULE_NATURAL_REGENERATION, null),
+	PROJECTILES_CAN_BREAK_BLOCKS(null, null),
+	REDUCED_DEBUG_INFO(GameRules.RULE_REDUCEDDEBUGINFO, null),
+	SEND_COMMAND_FEEDBACK(GameRules.RULE_SENDCOMMANDFEEDBACK, null),
+	SHOW_DEATH_MESSAGES(GameRules.RULE_SHOWDEATHMESSAGES, null),
+	SPECTATORS_GENERATE_CHUNKS(GameRules.RULE_SPECTATORSGENERATECHUNKS, null),
+	TNT_EXPLOSION_DROP_DECAY(null, null),
+	UNIVERSAL_ANGER(GameRules.RULE_UNIVERSAL_ANGER, null),
+	WATER_SOURCE_CONVERSION(null, null),
+	COMMAND_MODIFICATION_BLOCK_LIMIT(null, null),
+	MAX_COMMAND_CHAIN_LENGTH(null, GameRules.RULE_MAX_COMMAND_CHAIN_LENGTH),
+	MAX_COMMAND_FORK_COUNT(null, null),
+	MAX_ENTITY_CRAMMING(null, GameRules.RULE_MAX_ENTITY_CRAMMING),
+	PLAYERS_NETHER_PORTAL_CREATIVE_DELAY(null, null),
+	PLAYERS_NETHER_PORTAL_DEFAULT_DELAY(null, null),
+	PLAYERS_SLEEPING_PERCENTAGE(null, GameRules.RULE_PLAYERS_SLEEPING_PERCENTAGE),
+	RANDOM_TICK_SPEED(null, GameRules.RULE_RANDOMTICKING),
+	SNOW_ACCUMULATION_HEIGHT(null, null),
+	SPAWN_RADIUS(null, GameRules.RULE_SPAWN_RADIUS);
+
+
+	@Nullable
+	private final GameRules.Key<GameRules.BooleanValue> gameRuleBoolean;
+	@Nullable
+	private final GameRules.Key<GameRules.IntegerValue> gameRuleInteger;
+
+	@Deprecated
+	GameRule(@Nullable GameRules.Key<GameRules.BooleanValue> gameRuleBoolean, @Nullable GameRules.Key<GameRules.IntegerValue> gameRuleInteger) {
+		this.gameRuleBoolean = gameRuleBoolean;
+		this.gameRuleInteger = gameRuleInteger;
+	}
+
+	@MappedMethod
+	public boolean getBooleanGameRule(MinecraftServer minecraftServer) {
+		return gameRuleBoolean != null && minecraftServer.getGameRules().getBoolean(gameRuleBoolean);
+	}
+
+	@MappedMethod
+	public int getIntegerGameRule(MinecraftServer minecraftServer) {
+		return gameRuleInteger == null ? 0 : minecraftServer.getGameRules().getInt(gameRuleInteger);
+	}
+
+	@MappedMethod
+	public boolean hasBooleanGameRule() {
+		return gameRuleBoolean != null;
+	}
+
+	@MappedMethod
+	public boolean hasIntegerGameRule() {
+		return gameRuleInteger != null;
+	}
+}
