cae17647cd8f41f84b7b43aff3a086c2ddf49dd6
Jonathan Ho <jonafanho@gmail.com>
2024-04-22 23:09:27 -0700

    Fixed missing 1.19.2 override for EntityExtension

--- fabric/1.19.2-mapping/src/main/java/org/mtr/mapping/mapper/EntityExtension.java
+++ fabric/1.19.2-mapping/src/main/java/org/mtr/mapping/mapper/EntityExtension.java
@@ -1,16 +1,27 @@
 package org.mtr.mapping.mapper;
 
+import net.minecraft.network.Packet;
+import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import org.mtr.mapping.annotation.MappedMethod;
 import org.mtr.mapping.holder.CompoundTag;
 import org.mtr.mapping.holder.EntityAbstractMapping;
 import org.mtr.mapping.holder.EntityType;
 import org.mtr.mapping.holder.World;
 
+import javax.annotation.Nonnull;
+
 public abstract class EntityExtension extends EntityAbstractMapping {
 
 	@MappedMethod
 	public EntityExtension(EntityType<?> type, World world) {
 		super(type, world);
+	}
+
+	@Deprecated
+	@Nonnull
+	@Override
+	public final Packet<?> createSpawnPacket2() {
+		return new EntitySpawnS2CPacket(this);
 	}
 
 	@Deprecated
--- forge/1.19.2-mapping/src/main/java/org/mtr/mapping/mapper/EntityExtension.java
+++ forge/1.19.2-mapping/src/main/java/org/mtr/mapping/mapper/EntityExtension.java
@@ -1,16 +1,27 @@
 package org.mtr.mapping.mapper;
 
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import org.mtr.mapping.annotation.MappedMethod;
 import org.mtr.mapping.holder.CompoundTag;
 import org.mtr.mapping.holder.EntityAbstractMapping;
 import org.mtr.mapping.holder.EntityType;
 import org.mtr.mapping.holder.World;
 
+import javax.annotation.Nonnull;
+
 public abstract class EntityExtension extends EntityAbstractMapping {
 
 	@MappedMethod
 	public EntityExtension(EntityType<?> type, World world) {
 		super(type, world);
+	}
+
+	@Deprecated
+	@Nonnull
+	@Override
+	public final Packet<?> getAddEntityPacket2() {
+		return new ClientboundAddEntityPacket(this);
 	}
 
 	@Deprecated
