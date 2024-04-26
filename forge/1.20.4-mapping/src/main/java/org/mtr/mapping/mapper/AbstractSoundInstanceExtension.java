93e2afb7284285886e2fd255954f43c3f7a8b257
Jonathan Ho <jonafanho@gmail.com>
2024-04-26 12:59:21 -0700

    Moved createSoundEvent into a non-clientside class

--- forge/1.20.4-mapping/src/main/java/org/mtr/mapping/mapper/AbstractSoundInstanceExtension.java
+++ forge/1.20.4-mapping/src/main/java/org/mtr/mapping/mapper/AbstractSoundInstanceExtension.java
@@ -39,11 +39,6 @@ public abstract class AbstractSoundInstanceExtension extends AbstractSoundInstan
 	}
 
 	@MappedMethod
-	public static SoundEvent createSoundEvent(Identifier identifier) {
-		return SoundEvent.createVariableRangeEvent(identifier);
-	}
-
-	@MappedMethod
 	public static void iterateSoundIds(Consumer<Identifier> consumer) {
 		MinecraftClient.getInstance().getSoundManager().getAvailableSounds().forEach(identifier -> consumer.accept(new Identifier(identifier)));
 	}
