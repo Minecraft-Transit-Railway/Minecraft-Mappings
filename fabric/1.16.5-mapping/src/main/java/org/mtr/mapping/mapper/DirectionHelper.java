4b50a6aaaa57fea43d60d19796169a8b25d1ceeb
AIDA64S <zxc17634985169@gmail.com>
2024-05-20 00:36:41 +0800

    add omnidirectional FACING property

--- fabric/1.16.5-mapping/src/main/java/org/mtr/mapping/mapper/DirectionHelper.java
+++ fabric/1.16.5-mapping/src/main/java/org/mtr/mapping/mapper/DirectionHelper.java
@@ -1,9 +1,11 @@
 package org.mtr.mapping.mapper;
 
+import net.minecraft.block.FacingBlock;
 import net.minecraft.block.HorizontalFacingBlock;
 import org.mtr.mapping.holder.DirectionProperty;
 
 public interface DirectionHelper {
 
 	DirectionProperty FACING = new DirectionProperty(HorizontalFacingBlock.FACING);
+	DirectionProperty FACING_NORMAL = new DirectionProperty(FacingBlock.FACING);
 }
--- forge/1.18.2-mapping/src/main/java/org/mtr/mapping/mapper/DirectionHelper.java
+++ forge/1.18.2-mapping/src/main/java/org/mtr/mapping/mapper/DirectionHelper.java
@@ -1,9 +1,11 @@
 package org.mtr.mapping.mapper;
 
+import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import org.mtr.mapping.holder.DirectionProperty;
 
 public interface DirectionHelper {
 
 	DirectionProperty FACING = new DirectionProperty(HorizontalDirectionalBlock.FACING);
+	DirectionProperty FACING_NORMAL = new DirectionProperty(DirectionalBlock.FACING);
 }
