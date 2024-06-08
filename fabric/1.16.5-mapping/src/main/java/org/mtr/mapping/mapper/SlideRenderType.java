1b9d0dba3a7dc3c927cf6c33ca20ff69234e3b3a
AIDA64S <zxc17634985169@gmail.com>
2024-06-08 17:18:46 +0800

    add SlideRenderType(Don't pull request)

---
+++ fabric/1.16.5-mapping/src/main/java/org/mtr/mapping/mapper/SlideRenderType.java
@@ -0,0 +1,66 @@
+package org.mtr.mapping.mapper;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderPhase;
+import net.minecraft.client.render.VertexFormats;
+import org.lwjgl.opengl.GL11;
+import org.mtr.mapping.holder.Identifier;
+import org.mtr.mapping.holder.MinecraftClient;
+
+import java.util.Objects;
+
+public final class SlideRenderType extends RenderLayer {
+    private static final ImmutableList<RenderPhase> GENERAL_STATES;
+
+    static {
+        GENERAL_STATES = ImmutableList.of(
+                TRANSLUCENT_TRANSPARENCY,
+                DISABLE_DIFFUSE_LIGHTING,
+                SHADE_MODEL,
+                ONE_TENTH_ALPHA,
+                LEQUAL_DEPTH_TEST,
+                ENABLE_CULLING,
+                ENABLE_LIGHTMAP,
+                DISABLE_OVERLAY_COLOR,
+                FOG,
+                NO_LAYERING,
+                MAIN_TARGET,
+                DEFAULT_TEXTURING,
+                ALL_MASK,
+                FULL_LINE_WIDTH
+        );
+    }
+
+    private final int mHashCode;
+
+    public SlideRenderType(String modId, int texture) {
+        super(modId, VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL,
+                GL11.GL_QUADS, 256, false, true,
+                () -> {
+                    GENERAL_STATES.forEach(RenderPhase::startDrawing);
+                    RenderSystem.enableTexture();
+                    RenderSystem.bindTexture(texture);
+                },
+                () -> GENERAL_STATES.forEach(RenderPhase::endDrawing));
+        mHashCode = Objects.hash(super.hashCode(), GENERAL_STATES, texture);
+    }
+
+    public SlideRenderType(String modId, Identifier texture) {
+        super(modId, VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL,
+                GL11.GL_QUADS, 256, false, true,
+                () -> {
+                    GENERAL_STATES.forEach(RenderPhase::startDrawing);
+                    RenderSystem.enableTexture();
+                    MinecraftClient.getInstance().getTextureManager().bindTexture(texture);
+                },
+                () -> GENERAL_STATES.forEach(RenderPhase::endDrawing));
+        mHashCode = Objects.hash(super.hashCode(), GENERAL_STATES, texture);
+    }
+
+    @Override
+    public int hashCode() {
+        return mHashCode;
+    }
+}
---
+++ fabric/1.19.4-mapping/src/main/java/org/mtr/mapping/mapper/SlideRenderType.java
@@ -0,0 +1,55 @@
+package org.mtr.mapping.mapper;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
+import org.mtr.mapping.holder.Identifier;
+
+public final class SlideRenderType extends RenderLayer.MultiPhase {
+    public SlideRenderType(String modId, int texture) {
+        super(modId, VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL,
+                VertexFormat.DrawMode.QUADS, 256, false, true,
+                MultiPhaseParameters.builder()
+                        .program(TRANSPARENT_TEXT_PROGRAM)
+                        .transparency(TRANSLUCENT_TRANSPARENCY)
+                        .depthTest(LEQUAL_DEPTH_TEST)
+                        .cull(ENABLE_CULLING)
+                        .lightmap(ENABLE_LIGHTMAP)
+                        .overlay(DISABLE_OVERLAY_COLOR)
+                        .layering(NO_LAYERING)
+                        .target(MAIN_TARGET)
+                        .texturing(DEFAULT_TEXTURING)
+                        .writeMaskState(ALL_MASK)
+                        .lineWidth(FULL_LINE_WIDTH)
+                        .build(true));
+        var baseSetup = this.beginAction;
+        this.beginAction = () -> {
+            baseSetup.run();
+            RenderSystem.setShaderTexture(0, texture);
+        };
+    }
+
+    public SlideRenderType(String modId, Identifier texture) {
+        super(modId + "_icon", VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL,
+                VertexFormat.DrawMode.QUADS, 256, false, true,
+                MultiPhaseParameters.builder()
+                        .program(TRANSPARENT_TEXT_PROGRAM)
+                        .transparency(TRANSLUCENT_TRANSPARENCY)
+                        .depthTest(LEQUAL_DEPTH_TEST)
+                        .cull(ENABLE_CULLING)
+                        .lightmap(ENABLE_LIGHTMAP)
+                        .overlay(DISABLE_OVERLAY_COLOR)
+                        .layering(NO_LAYERING)
+                        .target(MAIN_TARGET)
+                        .texturing(DEFAULT_TEXTURING)
+                        .writeMaskState(ALL_MASK)
+                        .lineWidth(FULL_LINE_WIDTH)
+                        .build(true));
+        var baseSetup = this.beginAction;
+        this.beginAction = () -> {
+            baseSetup.run();
+            RenderSystem.setShaderTexture(0, texture.data);
+        };
+    }
+}
