4cbecce56c0bf8308f2609e6bc6d1cc7ece3ff5d
AIDA64S <zxc17634985169@gmail.com>
2024-06-27 19:22:07 +0800

    add `useWithoutResult` method in `org.mtr.mapping.mapper.BlockItemExtension.java`

--- fabric/1.17.1-mapping/src/main/java/org/mtr/mapping/mapper/BlockItemExtension.java
+++ fabric/1.17.1-mapping/src/main/java/org/mtr/mapping/mapper/BlockItemExtension.java
@@ -1,14 +1,25 @@
 package org.mtr.mapping.mapper;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.TypedActionResult;
 import org.mtr.mapping.annotation.MappedMethod;
-import org.mtr.mapping.holder.Block;
-import org.mtr.mapping.holder.BlockItemAbstractMapping;
-import org.mtr.mapping.holder.ItemSettings;
+import org.mtr.mapping.holder.*;
 
 public class BlockItemExtension extends BlockItemAbstractMapping {
 
-	@MappedMethod
-	public BlockItemExtension(Block block, ItemSettings itemSettings) {
-		super(block, itemSettings);
-	}
+    @MappedMethod
+    public BlockItemExtension(Block block, ItemSettings itemSettings) {
+        super(block, itemSettings);
+    }
+
+    @Deprecated
+    @Override
+    public final TypedActionResult<ItemStack> use(net.minecraft.world.World world, net.minecraft.entity.player.PlayerEntity user, net.minecraft.util.Hand hand) {
+        useWithoutResult(new World(world), new PlayerEntity(user), Hand.convert(hand));
+        return super.use(world, user, hand);
+    }
+
+    @MappedMethod
+    public void useWithoutResult(World world, PlayerEntity user, Hand hand) {
+    }
 }
