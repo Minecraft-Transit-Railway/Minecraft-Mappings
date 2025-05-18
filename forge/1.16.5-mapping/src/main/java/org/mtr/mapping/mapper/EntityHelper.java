9d5673ea1c979a29e4936e9b26dcff69929d9b99
Jonathan Ho <jonafanho@gmail.com>
2025-05-17 19:08:28 -0700

    Added the spawn item in world helper method

--- forge/1.16.5-mapping/src/main/java/org/mtr/mapping/mapper/EntityHelper.java
+++ forge/1.16.5-mapping/src/main/java/org/mtr/mapping/mapper/EntityHelper.java
@@ -1,7 +1,11 @@
 package org.mtr.mapping.mapper;
 
+import net.minecraft.entity.item.ItemEntity;
 import org.mtr.mapping.annotation.MappedMethod;
+import org.mtr.mapping.holder.BlockPos;
 import org.mtr.mapping.holder.Entity;
+import org.mtr.mapping.holder.ItemStack;
+import org.mtr.mapping.holder.ServerWorld;
 import org.mtr.mapping.tool.DummyClass;
 
 import java.util.ArrayList;
@@ -30,5 +34,10 @@ public final class EntityHelper extends DummyClass {
 	@MappedMethod
 	public static void setYaw(Entity entity, float yaw) {
 		entity.data.yRot = yaw;
+	}
+
+	@MappedMethod
+	public static void spawnItem(ServerWorld serverWorld, BlockPos blockPos, ItemStack itemStack) {
+		serverWorld.data.addFreshEntity(new ItemEntity(serverWorld.data, blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5, itemStack.data));
 	}
 }
